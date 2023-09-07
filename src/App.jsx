7200fad28c666d3744a4cca0aba04c9a11d8a315
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-07 15:41:09 +0200

    add basic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
 
 const App = () => {
+  const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+  const FadeUp = batch(Fade(), Move(), Sticky());
+
   return (
     <div>
-      <h1 class="bg-danger">Prova</h1>
+
+      <ScrollContainer>
+        <ScrollPage>
+          <Animator animation={FadeUp}>
+            <span style={{ fontSize: "40px" }}>I'm FadeUp ⛅️</span>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage className='bg-danger'>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }} >
+            <span style={{ fontSize: "40px" }}>
+              <Animator className='card my-2' animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator>
+              <Animator className='card my-2' animation={MoveIn(1000, 0)}>Nice to meet you 🙋🏻‍♀️</Animator>
+              <Animator className='card my-2' animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
+              <Animator className='card my-2' animation={MoveOut(-1000, 0)}>See you 💛</Animator>
+            </span>
+          </div>
+        </ScrollPage>
+      </ScrollContainer>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
